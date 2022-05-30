dbf09fce7002813b504972dcf1776da7d988b93b
KAKPONOTAM <sabitovdanill@icloud.com>
2022-05-30 17:56:28 +0300

    created_networkManager

--- CountriesInformation/Services/NetworkManager.swift
+++ CountriesInformation/Services/NetworkManager.swift
@@ -1,8 +1,29 @@
-//
-//  NetworkManager.swift
-//  CountriesInformation
-//
-//  Created by Сабитов Данил on 30.05.2022.
-//
-
 import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private init() {}
+    
+    func getCountriesData(page: Int, completion: @escaping (CountriesData) -> ()) {
+        let urlString = "https://rawgit.com/NikitaAsabin/799e4502c9fc3e0ea7af439b2dfd88fa/raw/7f5c6c66358501f72fada21e04d75f64474a7888/page\(page).json"
+        guard let url = URL(string: urlString) else { return }
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
+            if error == nil,
+               let data = data {
+                do {
+                    let countriesData = try JSONDecoder().decode(CountriesData.self, from: data)
+                    
+                    DispatchQueue.main.async {
+                        completion(countriesData)
+                    }
+                    
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+        
+        dataTask.resume()
+    }
+}
