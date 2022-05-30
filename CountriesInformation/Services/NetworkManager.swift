66c528261518820ea3bfe48fb5ecfc8eca25efad
KAKPONOTAM <sabitovdanill@icloud.com>
2022-05-30 18:09:50 +0300

    added_URLTypes_for_send_request

--- CountriesInformation/Services/NetworkManager.swift
+++ CountriesInformation/Services/NetworkManager.swift
@@ -4,8 +4,8 @@ class NetworkManager {
     static let shared = NetworkManager()
     private init() {}
     
-    func getCountriesData(page: Int, completion: @escaping (CountriesData) -> ()) {
-        let urlString = "https://rawgit.com/NikitaAsabin/799e4502c9fc3e0ea7af439b2dfd88fa/raw/7f5c6c66358501f72fada21e04d75f64474a7888/page\(page).json"
+    func getCountriesData(by urlStrings: URLStrings, completion: @escaping (CountriesData) -> ()) {
+        let urlString = urlStrings.stringURL
         guard let url = URL(string: urlString) else { return }
         
         let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
