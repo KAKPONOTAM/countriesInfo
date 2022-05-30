f7a9af1ab8ff7935494211bc66ff86e61c77b328
KAKPONOTAM <sabitovdanill@icloud.com>
2022-05-30 17:44:51 +0300

    created_Models

--- CountriesInformation/Models/CountriesData.swift
+++ CountriesInformation/Models/CountriesData.swift
@@ -1,8 +1,31 @@
-//
-//  CountriesData.swift
-//  CountriesInformation
-//
-//  Created by Сабитов Данил on 30.05.2022.
-//
-
 import Foundation
+
+struct CountriesData: Decodable {
+    let next: String
+    let countries: [Country]
+}
+
+struct Country: Decodable {
+    let name: String
+    let continent: String
+    let capital: String
+    let population: Int
+    let smallDescription: String
+    let countryDescription: String
+    let image: String
+    let countryInfo: CountryInfo
+    
+    enum CodingKeys: String, CodingKey {
+        case name, continent, capital, population
+        case smallDescription = "description_small"
+        case countryDescription = "description"
+        case image
+        case countryInfo = "country_info"
+    }
+}
+
+struct CountryInfo: Decodable {
+    let images: [String]
+    let flag: String
+}
+
