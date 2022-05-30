b1092d646334fd7fde7257e6aea09f332034299c
KAKPONOTAM <sabitovdanill@icloud.com>
2022-05-30 17:56:07 +0300

    created_CountriesViewController

--- CountriesInformation/UI/Controllers/CountriesViewController.swift
+++ CountriesInformation/UI/Controllers/CountriesViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class CountriesViewController: UIViewController {
+    private var countriesData: CountriesData?
+    
     private lazy var countriesViewController: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -10,15 +12,33 @@ class CountriesViewController: UIViewController {
         return tableView
     }()
     
+    init(countriesData: CountriesData) {
+        self.countriesData = countriesData
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        countriesData = nil
+        super.init(coder: coder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .white
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .systemGray6
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        title = "Countries"
     }
 }
 
 extension CountriesViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return countriesData?.countries.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
