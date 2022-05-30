ae200b83095193c49eb40746a467c5be709d4664
KAKPONOTAM <sabitovdanill@icloud.com>
2022-05-30 17:43:59 +0300

    created_CountriesViewController

---
+++ CountriesInformation/UI/Controllers/CountriesViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class CountriesViewController: UIViewController {
+    private lazy var countriesViewController: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+extension CountriesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
---
+++ CountriesInformation/Utilities/Extensions/UIView.swift
@@ -0,0 +1,8 @@
+//
+//  UIView.swift
+//  CountriesInformation
+//
+//  Created by Сабитов Данил on 30.05.2022.
+//
+
+import Foundation
