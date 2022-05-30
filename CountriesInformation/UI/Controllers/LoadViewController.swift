2ae946fcb48e51fd6513e0f0247ef3968224cf40
KAKPONOTAM <sabitovdanill@icloud.com>
2022-05-30 17:36:24 +0300

    added_UI

--- CountriesInformation/UI/Controllers/LoadViewController.swift
+++ CountriesInformation/UI/Controllers/LoadViewController.swift
@@ -1,10 +1,29 @@
 import UIKit
 
 class LoadViewController: UIViewController {
+    private let activityIndicator: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.startAnimating()
+        return spinner
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        addSubview()
+        setupConstraints()
+    }
+    
+    private func addSubview() {
+        view.addSubview(activityIndicator)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
 
