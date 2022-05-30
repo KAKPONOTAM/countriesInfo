831966456f67c3eff48a5fc00b88148b4dd73843
KAKPONOTAM <sabitovdanill@icloud.com>
2022-05-30 17:44:20 +0300

    created_countriesTableView

--- CountriesInformation/UI/Controllers/CountriesViewController.swift
+++ CountriesInformation/UI/Controllers/CountriesViewController.swift
@@ -6,6 +6,7 @@ class CountriesViewController: UIViewController {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.identifier)
         return tableView
     }()
     
@@ -21,6 +22,7 @@ extension CountriesViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.identifier, for: indexPath)
+        return cell
     }
 }
