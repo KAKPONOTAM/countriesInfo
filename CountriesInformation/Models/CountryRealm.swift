a28121af28b4594356e342a74d5a9a3a6a51e4ac
KAKPONOTAM <sabitovdanill@icloud.com>
2022-06-02 18:32:50 +0300

    created_model_for_realm

--- CountriesInformation/Models/CountryRealm.swift
+++ CountriesInformation/Models/CountryRealm.swift
@@ -2,16 +2,40 @@ import Foundation
 import RealmSwift
 
 @objcMembers
-class CountryRealm: Object {
-    dynamic var name: String
-    dynamic var continent: String
-    dynamic var capital: String
-    dynamic var population: Int
-    dynamic var smallDescription: String
-    dynamic var countryDescription: String
-    dynamic var image: String
+class CountriesRealmData: Object {
+    dynamic var next: String = ""
+    dynamic var key = "key"
+    dynamic var countries: Results<CountryRealm>? {
+        let realm = try? Realm()
+        return realm?.objects(CountryRealm.self)
+    }
     
-    init(name: String, continent: String, capital: String, population: Int, smallDescription: String, countryDescription: String, image: String) {
+    
+    convenience init(next: String) {
+        self.init()
+        self.next = next
+    }
+    
+    override class func primaryKey() -> String? {
+        return #keyPath(key)
+    }
+}
+
+@objcMembers
+class CountryRealm: Object {
+    dynamic var name: String = ""
+    dynamic var continent: String = ""
+    dynamic var capital: String = ""
+    dynamic var population: Int = 0
+    dynamic var smallDescription: String = ""
+    dynamic var countryDescription: String = ""
+    dynamic var image: String = ""
+    dynamic var flag: String = ""
+    dynamic var countryImageData: Data = Data()
+    dynamic var images = List<String>()
+    
+    convenience init(name: String, continent: String, capital: String, population: Int, smallDescription: String, countryDescription: String, image: String, flag: String, countryImageData: Data, images: List<String>) {
+        self.init()
         self.name = name
         self.continent = continent
         self.capital = capital
@@ -19,6 +43,9 @@ class CountryRealm: Object {
         self.smallDescription = smallDescription
         self.countryDescription = countryDescription
         self.image = image
+        self.flag = flag
+        self.countryImageData = countryImageData
+        self.images = images
     }
     
     override class func primaryKey() -> String? {
