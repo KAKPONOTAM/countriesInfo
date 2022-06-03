f4629411c675d0753f7c82c3ec5ac6e70fd55b3e
KAKPONOTAM <sabitovdanill@icloud.com>
2022-06-03 16:50:04 +0300

    added_cellTypes

---
+++ CountriesInformation/UI/CustomElements/CountryDescriptionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CountryDescriptionTableViewCell.swift
+//  CountriesInformation
+//
+//  Created by Сабитов Данил on 03.06.2022.
+//
+
+import UIKit
+
+class CountryDescriptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
